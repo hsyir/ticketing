3640aa59271f58083912cdf698ee1b2b92c76164
Hossein <hosseinyaghmaee@gmail.com>
2020-11-12 14:27:24 +0330

    Tests Requirements

---
+++ tests/TestCase.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Hsy\Ticketing\Tests;
+
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+   protected function setUp(): void
+   {
+       parent::setUp();
+   }
+
+    protected function getPackageProviders($app)
+    {
+
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+
+    }
+}
