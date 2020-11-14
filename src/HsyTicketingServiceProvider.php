71d9cbf74b0640721d23d3c3f0d7a6382d44eb7f
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2020-11-15 01:01:44 +0330

    on..

--- src/HsyTicketingServiceProvider.php
+++ src/HsyTicketingServiceProvider.php
@@ -17,7 +17,9 @@ class HsyTicketingServiceProvider extends ServiceProvider
 
     public function boot()
     {
-
+        $this->publishes([
+            __DIR__.'/../database/migrations/' => database_path('migrations')
+        ], 'migrations');
     }
 
     /**
